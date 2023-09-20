099d7ef6539f65d3308219f97ca42ad8aa5b34c0
juuhak <43062862+juuhak@users.noreply.github.com>
2023-09-21 01:01:26 +0300

    Fixed regex for new format
    matches youtube links without starting www. or m.

--- youtubetitle_v2.pl
+++ youtubetitle_v2.pl
@@ -14,10 +14,10 @@ sub youtube_query {
     my $url = shift;
     my $server = shift;
     my $target = shift;
-    
+
     my $ua = LWP::UserAgent->new(ssl_opts => { verify_hostname => 1 });
     $ua->agent("irssi/0.1");
-    
+
     my $res = $ua->get($url);
     print "HTTP status: ", $res->code();
     my $content_json = decode_json($res->content);
@@ -27,43 +27,46 @@ sub youtube_query {
     #If no video found
     if ($content_json->{pageInfo}->{totalResults} == 0) {
         $output = "Video not found.";
-    } else { #Video found, set the title as output
+	} 
+    else { #Video found, set the title as output
         $output = $content_json->{items}[0]->{snippet}->{title};
     }
-
-	if ($res->is_success()){
+    if ($res->is_success()) {
         $server->command("msg $target // $output");
 	} else {
-	    my $status_line = $target->status_line();
-	    $server->command("msg $target $status_line");
-	}
+    	my $status_line = $target->status_line();
+        $server->command("msg $target $status_line");
+    }
 }
 
 sub message_public {
     my ($server,$msg,$nick,$address,$target) = @_;
     if ($target ~~ CHANNELS) {
         my $id = -1;
-        if ($msg =~ /(www\.|m\.)youtube\.com\/watch\?.*v=(.{11})/) {
-	        if ($msg =~ /(www\.|m\.)(youtube\.com\/watch\?.*v=.{11})\S*( \*)/) {
-	            print "Not getting link";
-	            return;
-	        }
-	        $id = $2;
+
+        if ($msg =~ /(www\.|m\.)?youtube\.com\/watch\?.*v=(.{11})/) {
+            print join('', "Got a link with video id: ", $2);
+            if ($msg =~ /(www\.|m\.)?(youtube\.com\/watch\?.*v=.{11})\S*( \*)/) {
+                print "Not getting link";
+                return;
+            }
+            $id = $2;
         }
-    
+
         if ($msg =~ /https?\:\/\/(www.)?\youtu\.be\/(.{11})/) {
-	        if ($msg =~ /https?\:\/\/(www.)?\youtu\.be\/(.{11})\S*( \*)/) {
-	            print "Not getting link";
-	            return;
-	        }
-	        $id = $2;
+            print join('', "Got a link with video id: ", $2);
+            if ($msg =~ /https?\:\/\/(www.)?\youtu\.be\/(.{11})\S*( \*)/) {
+                print "Not getting link";
+                return;
+            }
+            $id = $2;
         }
 
         if ($id != -1) {
-	        my $query = join('',"https://www.googleapis.com/youtube/v3/videos?key=",API_KEY,"&part=snippet&id=",$id);
-	        print $query;
-	        youtube_query($query,$server,$target);
-	        return;
+            my $query = join('',"https://www.googleapis.com/youtube/v3/videos?key=",API_KEY,"&part=snippet&id=",$id);
+            print $query;
+            youtube_query($query,$server,$target);
+            return;
         }
     }
 }
